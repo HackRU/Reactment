9eb0aac188cda837d89cb2ff2cbdcbb6918ac7ea
Cole French <16979554+ColeFrench@users.noreply.github.com>
2020-01-07 21:30:50 -0500

    Animate Baby Yoda

--- challenge/src/mandalorian/BabyYoda.jsx
+++ challenge/src/mandalorian/BabyYoda.jsx
@@ -5,18 +5,68 @@ import "./BabyYoda.css";
 import strings from "../strings.json";
 
 export default class BabyYoda extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadState: 0
+        };
+    }
+
     render() {
         return (
             <div className="baby-yoda">
                 <div>
                     <h1>Touch This</h1>
                     <div>
-                        <img src={BabyYodaURI} alt={strings.babyYodaAlt} />
-                        <img src={ThugLifeGlassesURI} alt={strings.thugLifeGlassesAlt} />
+                        <img src={BabyYodaURI} alt={strings.babyYodaAlt} onLoad={this.onImageLoad} onMouseMove={this.onImageHover} />
+                        <img src={ThugLifeGlassesURI} alt={strings.thugLifeGlassesAlt} onLoad={this.onImageLoad} onMouseMove={this.onImageHover} />
                     </div>
                     <h1>You Cannot</h1>
                 </div>
             </div>
         );
     }
+
+    onImageLoad = () => {
+        this.setState(state => {
+            return {loadState: state.loadState + 1};
+        }, () => {
+            if (this.state.loadState === 2) {
+                this.animate();
+            }
+        });
+    };
+
+    animate = () => {
+        setTimeout(() => document.querySelector('.baby-yoda h1:first-child').style.opacity = 1, 250);
+        setTimeout(() => document.querySelector('.baby-yoda h1:last-child').style.opacity = 1, 500);
+        document.querySelector('.baby-yoda img:last-child').style.opacity = 1;
+    };
+
+    onImageHover = () => {
+        if (this.state.loadState === 2) {
+            const maxWidth = document.querySelector('.baby-yoda').getBoundingClientRect().width;
+            const container = document.querySelector('.baby-yoda > div');
+            const containerWidth = container.getBoundingClientRect().width;
+            let x = container.style.left ? parseFloat(container.style.left.slice(0, -1)) * maxWidth / 100 : parseFloat(getComputedStyle(container).left.slice(0, -2));
+
+            // const leftMin = 0;
+            const leftMax = x - containerWidth;
+            const rightMin = x + containerWidth;
+            const rightMax = maxWidth - containerWidth;
+
+            if (leftMax < 0) {
+                if (rightMax >= rightMin) {
+                    x = rightMin + Math.random() * (rightMax - rightMin);
+                }
+            } else if (rightMax < rightMin) {
+                x = Math.random() * leftMax;
+            } else {
+                x = Math.random() * (maxWidth - containerWidth) <= leftMax ? Math.random() * leftMax : rightMin + Math.random() * (rightMax - rightMin);
+            }
+
+            container.style.left = `${x / maxWidth * 100}%`;
+            container.style.transform = `translateX(-${container.style.left})`;
+        }
+    };
 }
