787d1301bfeaf467a6d5bde0d3b4b2e2b28264e2
Jason Cheng <jasoncheng3443@gmail.com>
2020-01-17 20:39:28 -0500

    Update Github Text

--- challenge/src/App.jsx
+++ challenge/src/App.jsx
@@ -1,45 +1,47 @@
 import React, { Component } from "react";
 import BabyYoda from "./mandalorian/BabyYoda";
-import NumberLogger from './NumberLogger';
+import NumberLogger from "./NumberLogger";
 import strings from "./strings.json";
-import Navbar from './components/Navbar'
-import Fade from 'react-reveal/Fade'
+import Navbar from "./components/Navbar";
+import Fade from "react-reveal/Fade";
 
 class App extends Component {
-    state = {}
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        console.log("Render Method is Working. Redered successfully! I think?")
-        return (
-            <>
-              <Navbar fixed={true} items={['Home', 'Sponsors', 'About', 'Contact us']} />
-              <div style={{ width: "100%", marginTop: "4rem", textAlign: "center" }}>
-                <Fade left cascade>
-                  {strings.greeting}
+  state = {};
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log("Render Method is Working. Redered successfully! I think?");
+    return (
+      <>
+        <Navbar
+          fixed={true}
+          items={["Home", "Sponsors", "About", "Contact us"]}
+        />
+        <div style={{ width: "100%", marginTop: "4rem", textAlign: "center" }}>
+          <Fade left cascade>
+            {strings.greeting}
 
-                  <form action="https://github.com/HackRU">
-                    <input type="submit" value="Go to GitHub!" />
-                  </form>
+            <form action="https://github.com/HackRU">
+              <input type="submit" value="Go to HackRU's GitHub!" />
+            </form>
 
-                  <NumberLogger/>
-                </Fade>
-
-              </div>
-              <BabyYoda />
-            </>
-        );
-    }
+            <NumberLogger />
+          </Fade>
+        </div>
+        <BabyYoda />
+      </>
+    );
+  }
 }
 
-const sum = (list) => {
-    let result = 0;
-    for (let i = 1; i < list.length; i++) {
-        result += list[i];
-    }
-    return result;
+const sum = list => {
+  let result = 0;
+  for (let i = 1; i < list.length; i++) {
+    result += list[i];
+  }
+  return result;
 };
 
 export default App;
-export {sum};
+export { sum };
