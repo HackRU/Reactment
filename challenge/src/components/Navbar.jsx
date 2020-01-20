bcc427b4a168a89791ecdf6b43e6da7beeb67484
benlambert43 <benlambert43@gmail.com>
2020-01-19 20:14:42 -0500

    Issue #25 Improved Nav (add a callback to the constructor of the Navbar that notifies the parent component)

--- challenge/src/components/Navbar.jsx
+++ challenge/src/components/Navbar.jsx
@@ -1,42 +1,55 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            focused: 0
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    render() {
-        var self = this;
-        const { focused } = self.state;
+  handleClick(index) {
+    this.props.onPageChange(index);
+  }
 
-        return (
-            <div className={this.props.fixed && "fixed"}>
-                <ul>
-                    {this.props.items.map(function (element, index) {
-                        var style = '';
-                        if (focused === index) {
-                            style = 'active';
-                        }
-                        return <li className={style} onClick={() => self.setState({ focused: index })}>{element}</li>;
-                    })}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    var self = this;
+    const focused = this.props.currentPage;
+
+    return (
+      <div className={this.props.fixed && "fixed"}>
+        <ul>
+          {this.props.items.map(function(element, index) {
+            var style = "";
+            if (focused === index) {
+              style = "active";
+            }
+            return (
+              <li
+                key={index}
+                className={style}
+                onClick={() => self.handleClick(index)}
+              >
+                {element}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {
-    fixed : PropTypes.bool ,
-    items : PropTypes.array
-}
+  fixed: PropTypes.bool,
+  items: PropTypes.array
+};
 
 Navbar.defaultPropTypes = {
-    fixed : true  ,
-    items : []
-}
+  fixed: true,
+  items: []
+};
 
 export default Navbar;
