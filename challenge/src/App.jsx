0d29670372a9f0d057187bf381989e0f62ecf617
benlambert43 <benlambert43@gmail.com>
2020-01-19 21:22:48 -0500

    Issue #25 Imrpoved Nav (add state to the app component to display different content when the user selects a different option on the nav bar)

--- challenge/src/App.jsx
+++ challenge/src/App.jsx
@@ -17,9 +17,233 @@ class App extends Component {
     this.setState({ currentPage: index });
   }
 
+  Router(param) {
+    console.log(param.PageRoute);
+    switch (param.PageRoute) {
+      case 0:
+        return (
+          <div>
+            <div
+              style={{ width: "100%", marginTop: "4rem", textAlign: "center" }}
+            >
+              <Fade left cascade>
+                {strings.greeting}
+
+                <form action="https://github.com/HackRU">
+                  <input type="submit" value="Go to GitHub!" />
+                </form>
+
+                <NumberLogger />
+              </Fade>
+            </div>
+            <BabyYoda />
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <h1 style={{ fontSize: 100, color: "#FFC436" }}>
+              <center>Sponsors Page</center>
+            </h1>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+            <div
+              style={{
+                backgroundColor: "#115E7C",
+                display: "flex",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row"
+              }}
+            >
+              <img
+                src={"https://picsum.photos/id/" + param.PageRoute * 5 + "/500"}
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 10 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 20 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+            </div>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <h1 style={{ fontSize: 100, color: "#FFF5B8" }}>
+              <center>About Page</center>
+            </h1>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+            <div
+              style={{
+                backgroundColor: "#6693AC",
+                display: "flex",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row"
+              }}
+            >
+              <img
+                src={"https://picsum.photos/id/" + param.PageRoute * 5 + "/500"}
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 10 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 20 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+            </div>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            {" "}
+            <h1 style={{ fontSize: 100, color: "#F09F70" }}>
+              <center>Contact Page</center>
+            </h1>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+            <div
+              style={{
+                backgroundColor: "#546882",
+                display: "flex",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row"
+              }}
+            >
+              <img
+                src={"https://picsum.photos/id/" + param.PageRoute * 5 + "/500"}
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 10 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+              <img
+                src={
+                  "https://picsum.photos/id/" + param.PageRoute * 20 + "/500"
+                }
+                alt="picture"
+                height="20%"
+                width="20%"
+                style={{
+                  marginTop: "5%",
+                  marginBottom: "5%",
+                  marginRight: "5%",
+                  marginLeft: "5%"
+                }}
+              ></img>
+            </div>
+            <div style={{ backgroundColor: "whitesmoke" }}>
+              <br />
+            </div>
+          </div>
+        );
+      default:
+        return <div>An error occured.</div>;
+    }
+  }
+
   render() {
     console.log("User has selected page # ", this.state.currentPage);
     //console.log("Render Method is Working. Redered successfully! I think?")
+
     return (
       <>
         <Navbar
@@ -28,18 +252,7 @@ class App extends Component {
           onPageChange={this.handlePageChange}
           currentPage={this.state.currentPage}
         />
-        <div style={{ width: "100%", marginTop: "4rem", textAlign: "center" }}>
-          <Fade left cascade>
-            {strings.greeting}
-
-            <form action="https://github.com/HackRU">
-              <input type="submit" value="Go to GitHub!" />
-            </form>
-
-            <NumberLogger />
-          </Fade>
-        </div>
-        <BabyYoda />
+        <this.Router PageRoute={this.state.currentPage} />
       </>
     );
   }
