993fafea8c12990ea5f95962d201e0497841183b
Lincoln Roth <lincolnmroth@gmail.com>
2020-06-08 14:10:06 -0400

    add sponsor form

---
+++ challenge/src/components/SponsorForm.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+class SponsorForm extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {name: '',amount: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  handleSubmit(event) {
+    console.log('Name:' + this.state.name);
+    console.log('Amount: $' + this.state.amount);
+    event.preventDefault();
+  }
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "row"
+        }}>
+        DONATE NOW:
+        </div>
+        <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "row"
+        }}>
+          <label>Name:</label>
+          <input type="text" name="name" onChange={this.handleChange} />
+        <label>Amount:</label>
+        <input type="number" name="amount" onChange={this.handleChange} />
+        </div>
+        <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "row"
+        }}>
+        <input type="submit" value="Become A Sponsor Now!" />
+        </div>
+      </form>
+    );
+  }
+}
+export default SponsorForm
