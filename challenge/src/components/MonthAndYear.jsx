500420c1d3e82943a2ee8a7157327951d45dafaa
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-06-08 16:07:30 -0400

    Semester has begun fix

--- challenge/src/components/MonthAndYear.jsx
+++ challenge/src/components/MonthAndYear.jsx
@@ -7,9 +7,11 @@ export default class MonthAndYear extends React.Component {
         var currentDate = new Date();
         var fallSemester = new Date("09/1/2020");
         // Gets difference of fallsemester and current date
-        var result = Math.round((fallSemester.getTime() - currentDate.getTime()) / (one_day)) + 1; 
-        if(result >= 0){
+        var result = Math.round((fallSemester.getTime() - currentDate.getTime()) / (one_day)); 
+        if(result > 0){
             return result + " days until Fall 2020 Begins!";
+        }else if(result == 0){
+            return "Semester has begun";
         }else if(result < 0){
             return "Semester In Progress";
         }
