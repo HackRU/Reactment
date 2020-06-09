4639a4c2cc783c70b0428a5d7adc3ce54c77f914
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-06-09 16:43:14 -0400

    Added animations

--- challenge/src/components/JojoStand.jsx
+++ challenge/src/components/JojoStand.jsx
@@ -1,25 +1,43 @@
 import React from 'react'
 
-const JojoStand = (props) => {
-    var stands = {
-        "The World": "https://static.jojowiki.com/images/c/cb/TheWorldMangaAv.png",
-        "Hermit Purple": "https://static.jojowiki.com/images/0/08/HermitPurpleMangaAv.png",
-        "Magician's Red": "https://static.jojowiki.com/images/7/7c/MagicianRedMangaAv.png",
-        "Star Platinum": "https://cdn.discordapp.com/attachments/701574644595032104/710625607503052800/StarPlatinumMangaAv.png"
+class JojoStand extends React.Component{
+    constructor(props){
+        super(props);
     }
-return(
-    <div style= {props.style}>
-        <center>
-        <img
-            src={stands[props.stand]}
-            alt="RandomizedPicsum"
-            height="75%"
-            width="75%"
-        ></img>
-        <p>{props.stand}</p>
-        </center>
-    </div>
-)
+    visibility = () => {
+        if(!this.props.isVisible){
+            return "scale-down-center"
+        }else{
+            return ""
+        }
+    }
+    render(){
+        const standStyling = {
+            margin: "5%",
+            marginTop: "1%",
+            marginBottom: "0",
+            width: "25%"
+        }
+        var stands = {
+            "The World": "https://static.jojowiki.com/images/c/cb/TheWorldMangaAv.png",
+            "Hermit Purple": "https://static.jojowiki.com/images/0/08/HermitPurpleMangaAv.png",
+            "Magician's Red": "https://static.jojowiki.com/images/7/7c/MagicianRedMangaAv.png",
+            "Star Platinum": "https://cdn.discordapp.com/attachments/701574644595032104/710625607503052800/StarPlatinumMangaAv.png"
+        }
+        return(
+            <div style={standStyling} className={this.visibility()}>
+                <center>
+                    <img
+                        src={stands[this.props.stand]}
+                        height="50%"
+                        width="50%"
+                    ></img>
+                    <p>{this.props.stand}</p>
+                </center>
+            </div>
+        )
+    
+    }
 }
 
 export default JojoStand
