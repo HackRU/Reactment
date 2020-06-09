6f9fc48900f77cc42ac3d24c93ae4113fef2fda3
Jack Foltz <jack@foltz.io>
2020-06-09 16:53:15 -0400

    Add send and response logic

--- challenge/src/chatbox/ChatBox.jsx
+++ challenge/src/chatbox/ChatBox.jsx
@@ -4,16 +4,29 @@ import styles from './ChatBox.module.css';
 import ChatMessage from './ChatMessage';
 
 export default class ChatBox extends Component {
-    state = {
-        messages: [{
-            from: 'Bot',
-            message: "I didn't quite understand that, please try rephrasing what you said.",
-            date: new Date().toLocaleTimeString(),
-        }, {
-            from: 'You',
-            message: 'Help!',
-            date: new Date().toLocaleTimeString(),
-        }],
+    state = {messages: [], question: ''}
+
+    onChange = e => this.setState({question: e.target.value})
+    onSend = () => {
+        if (this.state.question == '')
+            return;
+
+        this.setState(state => ({
+            messages: [{
+                from: 'You',
+                message: state.question,
+                date: new Date().toLocaleTimeString(),
+            }, ...state.messages],
+            question: '',
+        }));
+
+        setTimeout(() => this.setState(state => ({
+            messages: [{
+                from: 'ReactMent Bot',
+                message: "I didn't quite understand that, please try rephrasing what you said.",
+                date: new Date().toLocaleTimeString(),
+            }, ...state.messages],
+        })), 500);
     }
 
     render() {
@@ -24,8 +37,13 @@ export default class ChatBox extends Component {
                 {this.state.messages.map(msg => (<ChatMessage {...msg} />))}
               </div>
               <div className={styles.footer}>
-                <input type='text' placeholder='Enter your question...' />
-                <span>&gt;</span>
+                <input
+                  type='text'
+                  placeholder='Enter your question...'
+                  value={this.state.question}
+                  onChange={this.onChange}
+                  onKeyPress={e => e.key == 'Enter' && this.onSend()} />
+                <span onClick={this.onSend}>&gt;</span>
               </div>
             </div>
         );
