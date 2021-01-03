30bc133adc32cc59cf4d058c188b3e7b7cd59c95
Raymond Phillips <phillips.n.ray@gmail.com>
2021-01-03 01:56:52 -0500

    Update index.js
    added the arrow function to the main method in index.js

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-function main(useServiceWorker) {
+let main = (useServiceWorker) => {
     ReactDOM.render(<App />, document.getElementById("root"));
     if (useServiceWorker) {
         serviceWorker.register();
