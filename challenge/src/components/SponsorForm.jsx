d0e7ce8eab2240fd2d1395a0f32d3f8e046d062f
MugPand <mugpand@gmail.com>
2021-01-03 18:24:40 -0500

    sponsor

--- challenge/src/components/SponsorForm.jsx
+++ challenge/src/components/SponsorForm.jsx
@@ -4,7 +4,7 @@ class SponsorForm extends React.Component {
 
   constructor () {
     super();
-    this.state = {name: '',amount: ''};
+    this.state = {name: '',amount: '',submittedName: '',rating: 0, show: false};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -13,8 +13,22 @@ class SponsorForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   }
   handleSubmit(event) {
+    this.setState({show: true, submittedName: this.state.name});
     console.log('Name:' + this.state.name);
     console.log('Amount: $' + this.state.amount);
+
+    if(this.state.amount >= 100) {
+      this.setState({rating: 5});
+    } else if (this.state.amount >= 50) {
+      this.setState({rating: 4});
+    }  else if (this.state.amount >= 25) {
+      this.setState({rating: 3});
+    } else if (this.state.amount >= 10) {
+      this.setState({rating: 2});
+    } else {
+      this.setState({rating: 1});
+    }
+
     event.preventDefault();
   }
   render () {
@@ -52,6 +66,9 @@ class SponsorForm extends React.Component {
           flexDirection: "row"
         }}>
         <input type="submit" value="Become A Sponsor Now!" />
+        <p>&nbsp;</p>
+        {this.state.show && <p>{this.state.rating} Star Sponsor:&nbsp; </p>}
+        {this.state.show && <p> {this.state.submittedName}</p>}
         </div>
       </form>
     );
