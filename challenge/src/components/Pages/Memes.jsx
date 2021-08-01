6101442e2b37f869271586426ab757a510b28a4f
rzmk <mueez25@gmail.com>
2021-08-01 18:02:51 -0400

    Memes stored in local files
    Create memes list only on component load

--- challenge/src/components/Pages/Memes.jsx
+++ challenge/src/components/Pages/Memes.jsx
@@ -1,9 +1,31 @@
 import React from "react"
+import meme1 from "../Memes/1.png"
+import meme2 from "../Memes/2.png"
+import meme3 from "../Memes/3.png"
+import meme4 from "../Memes/4.png"
+import meme5 from "../Memes/5.png"
+import meme6 from "../Memes/6.png"
+import meme7 from "../Memes/7.png"
+import meme8 from "../Memes/8.png"
+import meme9 from "../Memes/9.png"
+import meme10 from "../Memes/10.png"
 
 function Memes() {
+	const memes = [
+		meme1,
+		meme2,
+		meme3,
+		meme4,
+		meme5,
+		meme6,
+		meme7,
+		meme8,
+		meme9,
+		meme10,
+	]
 	const getRandomMeme = () => {
-		const randomNum = Math.floor(Math.random() * 10)
-		return randomNum
+		const randomMeme = memes[Math.floor(Math.random() * 10)]
+		return randomMeme
 	}
 
 	return (
@@ -13,7 +35,7 @@ function Memes() {
 				<div className="meme-image">
 					<center>
 						<img
-							src={`https://raw.githubusercontent.com/rzmk/reactment-memes/master/${getRandomMeme()}.png`}
+							src={getRandomMeme()}
 							alt="Meme"
 							style={{
 								width: "100%",
