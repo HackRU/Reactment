50e676a20224d43c35f45925691196ff9ccc2c5e
5H1V <spatel0240@gmail.com>
2021-08-02 03:58:03 -0400

    edit

---
+++ challenge/src/components/Mando.jsx
@@ -0,0 +1,13 @@
+import MandoURI from './images/mando.jpg';
+
+class Mando extends Component {
+	render() {
+		return (
+			<div>
+                  <img src={MandoURI} alt={"Mando"} />
+			</div>
+		);
+	}
+}
+
+export default Mando;
