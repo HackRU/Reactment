5102f16db0aeb52ed2c86d9af491d741b547d0fb
Andrew Heo <andrewheo@Andrews-MacBook-Pro.local>
2022-07-30 16:22:12 -0400

    done
    done

--- challenge/src/components/Jokes.jsx
+++ challenge/src/components/Jokes.jsx
@@ -1,68 +1,88 @@
-import React, { Component } from 'react';
-import dislikeImg from './images/dislike.png';
-import likeImg from './images/like.png';
-import jokeList from './jokesData.json';
+import React, { useState, Component } from "react";
+import dislikeImg from "./images/dislike.png";
+import likeImg from "./images/like.png";
+import jokeList from "./jokesData.json";
 
 class Jokes extends Component {
-    
-	constructor(props) {
-        super(props);
-        const jokes = jokeList.list;
-        const randomJoke = jokes[parseInt(Math.random() * jokes.length)]; 
-        this.state = {
-            textArea: randomJoke
-        };
-        this.dislikeClick = this.dislikeClick.bind(this);
-      }
-    
-    dislikeClick(e) {
-        const jokes = jokeList.list;
-        const randomJoke = jokes[parseInt(Math.random() * jokes.length)]; 
+  constructor(props) {
+    super(props);
+    const jokes = jokeList.list;
+    const randomJoke = jokes[parseInt(Math.random() * jokes.length)];
+    this.state = {
+      textArea: randomJoke,
+      count: 0,
+    };
+    this.dislikeClick = this.dislikeClick.bind(this);
+    this.increaseClick = this.increaseClick.bind(this);
+    this.decreaseClick = this.decreaseClick.bind(this);
+  }
 
-        this.setState({
-            textArea: randomJoke
-        });
-        
-    }
+  increaseClick() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  decreaseClick() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
 
-	render() {
-		return (
-            <>
-                <div id='generator' 
-                    style={{
-                        textAlign: 'center',
-                        color: 'white',
-                        fontSize: 10
-                    }}
-                >
-                    <h1 value='textArea' style={{textAlign: 'center', color: 'white', fontSize: 16}}>
-                        <center>
-                            {this.state.textArea}
-                        </center>
-                    </h1>
-                </div>
+  dislikeClick(e) {
+    const jokes = jokeList.list;
+    const randomJoke = jokes[parseInt(Math.random() * jokes.length)];
+    this.decreaseClick();
+   
+    this.setState({
+      textArea: randomJoke,
+    });
+  }
 
-                <h2 style={{ fontSize: 18, color: "#A9A9A9"}}>
-                    <center>Haha, did you get it, ha, ha...</center>
-                </h2>
+  render() {
+    return (
+      <>
+        <div
+          id="generator"
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontSize: 10,
+          }}
+        >
+          <h1
+            value="textArea"
+            style={{ textAlign: "center", color: "white", fontSize: 16 }}
+          >
+            <center>{this.state.textArea}</center>
+          </h1>
+        </div>
 
-                <div id='thumbs'>
-                    <center>
-                        <img 
-                            width='100px'
-                            src={likeImg}
-                            alt='Like button'
-                        />
-                        <img 
-                            width='100px'
-                            src={dislikeImg} 
-                            alt='Dislike button'
-                            onClick={this.dislikeClick} />
-                    </center>
-                </div>
-                
-            </>
-		);
-	}
+        <h2 style={{ fontSize: 18, color: "#A9A9A9" }}>
+          <center>Haha, did you get it, ha, ha...</center>
+        </h2>
+
+        <div id="thumbs">
+          <center>
+            <img
+              width="100px"
+              src={likeImg}
+              alt="Like button"
+              onClick={this.increaseClick}
+            />
+            <img
+              width="100px"
+              src={dislikeImg}
+              alt="Dislike button"
+              onClick={this.dislikeClick}
+            />
+          </center>
+          {/* I did inline css cuz too much work to make a css page ;) */}
+          <div>
+            <h2 style={{textAlign:'center'}}>The count is: {this.state.count}</h2>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 export default Jokes;
