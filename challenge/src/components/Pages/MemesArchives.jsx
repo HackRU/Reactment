4017e57c52f5cb5daa727f4e3c6a05809d538c9b
Anirudh Pappu <anirudh.pappu007@gmail.com>
2023-07-25 22:56:05 -0400

    (2) Meme Wrangling Fixes #171

---
+++ challenge/src/components/Pages/MemesArchives.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import meme1 from "../Memes/1.png"
+import meme2 from "../Memes/2.png"
+import meme3 from "../Memes/3.png"
+import meme4 from "../Memes/4.png"
+import meme5 from "../Memes/5.png"
+import meme6 from "../Memes/6.png"
+import meme7 from "../Memes/7.png"
+import meme8 from "../Memes/8.png"
+import meme9 from "../Memes/9.png"
+import meme10 from "../Memes/10.png"
+import BabyYoda from "../../mandalorian/BabyYoda";
+import PartyParrot from "../PartyParrot";
+import Mando from "../Mando";
+import ElonMusk from "../ElonMusk";
+import Maguire from '../Maguire';
+import FriesWithSauce from '../FriesWithSauce';
+import GIF from "../Gif"
+
+const Grid = () => {
+    return (
+      <div>
+			<h1 style={{ fontSize: 100, color: "#FFF5B8" }}>
+				{/* <center>Memes!</center>
+				<div className="meme-image">
+					Hello
+				</div> */}
+			</h1>
+		</div>
+    );
+  };
+  
+  export default Grid;
+
+// export default Memes
